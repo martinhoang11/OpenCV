ab1468d0f712c34aecaf91cd779949491d08e733
martinhoang11 <martinhoang11@gmail.com>
2020-06-01 21:22:56 +0700

    get started

--- get_started/arguments_image.py
+++ get_started/arguments_image.py
@@ -9,7 +9,6 @@ args = vars(ap.parse_args())
 
 #read image
 image = cv2.imread(args["input"])
-print(image)
 #convert to grayscale and blur image
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
